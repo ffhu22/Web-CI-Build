76e52d5af0ec7d940041440b2cfc2c13af8f180c
ffhu22 <fangfanghuu@163.com>
2020-05-01 23:06:42 +0800

    Add junit test structure, add junittest to build.xml.

---
+++ qa_assignment/unittest/com/sample/test/LoginTest.java
@@ -0,0 +1,15 @@
+package com.sample.test;
+ 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class LoginTest {
+	@Test
+	public void test1() {
+		assertEquals("One", "Two");
+	}
+	@Test
+	public void test2() {
+		System.out.println("this is test2....");
+	}
+}
